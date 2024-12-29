272fe9be1be64c93d9efdb10c5d1b3119a5ef363
hadeelwadia <hadeelwadia@gmail.com>
2024-12-29 13:51:43 +0200

    create header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,17 +4,22 @@ import logo from '../../assets/edutrack.png'
 const Header = () => {
   return (
     <header>
-      <section className='logo-contineer'>
-   <section>
-   <a href=''>
-        <img src={logo} alt='eductrack logo' className='logo-app'/>
-        </a>
-        <h1 className='name-app'>eduTrack Shcoole</h1>
+      <section className='logo-container'>
+     <section>
+      <a href='/'>
+        <img src={logo} alt='eductrack logo' className='logo-app'/></a>
+        <h1 className='name-app'>EduTrack Shcoole</h1>
    </section>
+
       </section>
 
-        <ul>
-          <li><a href='/'>home</a></li>
+        <ul className='main-links'>
+          <li ><a href='/'>dashboard</a></li>
+          <li><a href='/'>teathers</a></li>
+          <li><a href='/'>billing</a></li>
+          <li><a href='/'>exams</a></li>
+          <li><a href='/'>sitting&&profile</a></li>
+
         </ul>
     </header>
   )
