a4a145b5f7a75306523c59e5d5319a7c51146b92
hadeelwadia <hadeelwadia@gmail.com>
2024-12-29 13:21:43 +0200

    create-sutrcture-app

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,21 @@
 import { RouterProvider } from 'react-router-dom';
 import './App.css'
 import { routes } from './routes';
+import Header from './components/header';
 
 
 
 
 function App() {
   return (
-    <main>
-       <RouterProvider 
-          router={routes}/>
+    <>
+    <Header/>
+     <main>
+       <RouterProvider router={routes}/>
     </main>
+
+    </>
+
   )
 }
 
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
-
+import './header.css'
+import logo from '../../assets/edutrack.png'
 const Header = () => {
   return (
-    <div>
-        
-    </div>
+    <header>
+      <section className='logo-contineer'>
+   <section>
+   <a href=''>
+        <img src={logo} alt='eductrack logo' className='logo-app'/>
+        </a>
+        <h1 className='name-app'>eduTrack Shcoole</h1>
+   </section>
+      </section>
+
+        <ul>
+          <li><a href='/'>home</a></li>
+        </ul>
+    </header>
   )
 }
 
